80c3a1efa23453514c65d3f2e44e8f0d91fe8a33
thojm <thojmr@gmail.com>
2021-09-03 12:23:41 -0400

    Added KKS but KKAPI is throwing a fit

--- TestPlugin/TestPlugin.Core/TestPlugin.cs
+++ TestPlugin/TestPlugin.Core/TestPlugin.cs
@@ -35,64 +35,6 @@ namespace KK_TestPlugin
     public class TestPluginCharaController: CharaCustomFunctionController
     {  
 
-        //To reproduce in HS2
-        //  Open Maker and select any character
-        //  See that the output is different for the method called by update vs. coroutine
-        public bool triggerMeasureFromUpdate = false;
-
-        protected override void OnReload(GameMode currentGameMode)
-        {
-            TestPlugin.Logger.LogWarning($" Reload() Started");
-            StartCoroutine(ITakeMeasurement());
-        }
-
-        protected override void Update()
-        {
-            if (triggerMeasureFromUpdate)
-            {
-                TakeMeasurement("Update");
-                triggerMeasureFromUpdate = false;
-            }
-        }
-
-        public IEnumerator ITakeMeasurement() 
-        {
-            //Wait 2 seconds after Reload() is triggered 
-            yield return new WaitForSeconds(2f);
-
-            //Take measurement from coroutine
-            TakeMeasurement("ITakeMeasurement");
-
-            //trigger Update() to also take measurement at the same time (roughly)
-            triggerMeasureFromUpdate = true;
-        }
-
-        public void TakeMeasurement(string triggeredBy) 
-        {
-            var thighLName = "cf_J_LegUp00_L";
-            var thighRName = "cf_J_LegUp00_R";
-
-            //Get the characters hip bones to measure distance
-            var thighLBone = ChaControl.GetComponentsInChildren<Transform>().FirstOrDefault(x => x.name == thighLName);
-            var thighRBone = ChaControl.GetComponentsInChildren<Transform>().FirstOrDefault(x => x.name == thighRName);
-            if (thighLBone == null || thighRBone == null) return;
-            
-            //Measures Left to right hip bone distance and logs result
-            var waistWidth = Vector3.Distance(thighLBone.transform.InverseTransformPoint(thighLBone.position), thighLBone.transform.InverseTransformPoint(thighRBone.position));
-
-            // WHY IS THE MEASUREMENT DIFFERENT FROM COROUTINE THAN FROM UPDATE!
-            TestPlugin.Logger.LogWarning($" MeasureWaist result from {triggeredBy} : {waistWidth}");
-        }
-
-
-
-
-
-
-
-
-
-
         //Ignore
         protected override void OnCardBeingSaved(GameMode currentGameMode)
         {
@@ -103,5 +45,21 @@ namespace KK_TestPlugin
         { 
             base.Start();
         }
+
+        protected override void OnReload(GameMode currentGameMode)
+        {
+            TestPlugin.Logger.LogWarning($" Reload() Started");
+        }
+
+        protected override void Update()
+        {
+
+        }
+
+
+
+
+
+
     }
 }
